35409d177d905d936a10bbda2b4d68098c2a49be
Anastasia <mrgabrielripper@gmail.com>
2022-07-27 20:22:18 +0600

    Replace logo in footer with a dynamic one from wordpress admin panel

--- header.php
+++ header.php
@@ -25,11 +25,14 @@
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-11 col-sm-12 col-md-12 col-lg-3">
-                        <div class="header__logo">
-                            <?php
-                                the_custom_logo();
-                            ?>
-                        </div>
+                        <a href="<?php echo get_home_url(); ?>" class="header__logo">
+                            <img src="<?php 
+                            // получаем ссылку на логотип
+                            $custom_logo__url = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' ); 
+                            // выводим
+                            echo $custom_logo__url[0];
+                            ?>" alt="Мир детства" class="header__logo-img">
+                        </a>
                     </div>
                     <div class="col-md-12 col-lg-9 offset-lg-0 col-xl-7 offset-xl-2 ">
                         <div class="header__contacts">
