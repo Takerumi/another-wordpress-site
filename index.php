04130c63971316274efff2bf3efe6bb7b6d60af3
Anastasia <mrgabrielripper@gmail.com>
2022-07-27 21:37:09 +0600

    Create custom fields for dynamic filling company data from admin panel

--- index.php
+++ index.php
@@ -74,15 +74,22 @@
                 <div class="row">
                     <div class="col-md-10 offset-md-1 col-lg-5 offset-lg-1">
                         <div class="about__img">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/about.jpg" alt="про компанию">
+                            <!-- извлекаем url и alt картинки из custom field массива с изображением -->
+                            <?php
+                                    $image = get_field('about_img');
+
+                                    if (!empty($image)): ?>
+                                        <img 
+                                        src="<?php echo $image['url']; ?>" 
+                                        alt="<?php echo $image['alt']; ?>">
+                                    <?php endif;
+                                ?>
                         </div>
                     </div>
                     <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-6 col-xl-5 offset-xl-1">
-                        <h1 class="title underlined">Про компанию</h1>
+                        <h1 class="title underlined"><?php the_field('about_title'); ?></h1>
                         <div class="about__text">
-                            Наша компания уже больше десяти лет дарит позитивные эмоции детям и их родителям. Мы воплощаем все детские мечты и помогаем родителям дарить счастливое детство!
-                            <br> <br>
-                            Но и взрослые иногда так нуждаются в детских эмоциях! Мы можем и это, ведь так приятно почувствовать заботу даже когда тебе за... :)
+                        <?php the_field('about_descr'); ?>
                         </div>
                         <a href="#" class="button">Узнать больше</a>
                     </div>
